5bf9fd07e0f070e837e8752286deda2dee967f80
Purpose Knight <pranay.vshah06@gmail.com>
2025-12-23 00:37:23 +0530

    Initialize database on application startup

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI
 from models import JobApplication
+from database import create_table
 
 app = FastAPI(title="Placement Job Tracker API")
 
-applications = []
+create_table()
 
 @app.get("/")
 def home():
     return {"message": "API is running"}
-
-@app.post("/applications")
-def add_application(app_data: JobApplication):
-    applications.append(app_data)
-    return {"message": "Application added"}
-
-@app.get("/applications")
-def get_applications():
-    return applications
