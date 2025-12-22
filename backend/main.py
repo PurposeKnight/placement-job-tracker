0146bcf416e8ca6906c82269bd0c2fa0c7f893bf
Purpose Knight <pranay.vshah06@gmail.com>
2025-12-23 00:41:06 +0530

    Add POST API to store applications in database

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from models import JobApplication
-from database import create_table
+from database import get_connection, create_table
 
 app = FastAPI(title="Placement Job Tracker API")
 
@@ -9,3 +9,26 @@ create_table()
 @app.get("/")
 def home():
     return {"message": "API is running"}
+
+@app.post("/applications")
+def add_application(app: JobApplication):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO applications 
+        (company, role, location, status, applied_date, notes)
+        VALUES (?, ?, ?, ?, ?, ?)
+    """, (
+        app.company,
+        app.role,
+        app.location,
+        app.status,
+        app.applied_date,
+        app.notes
+    ))
+
+    conn.commit()
+    conn.close()
+
+    return {"message": "Application added successfully"}
