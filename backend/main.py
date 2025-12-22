e678d8fa523fc2481000fd36571037abe0a2bbb9
Purpose Knight <pranay.vshah06@gmail.com>
2025-12-23 00:26:03 +0530

    Add POST API to create job application

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from models import JobApplication
 
 app = FastAPI(title="Placement Job Tracker API")
+
+applications = []
 
 @app.get("/")
 def home():
     return {"message": "API is running"}
+
+@app.post("/applications")
+def add_application(app_data: JobApplication):
+    applications.append(app_data)
+    return {"message": "Application added"}
